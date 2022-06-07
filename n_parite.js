de1f6eb844009066881a536098113fee63b33666
Arick Bulakali <85836702+NdekoCode@users.noreply.github.com>
2022-06-07 14:23:32 +0000

    finish exercices

--- n_parite.js
+++ n_parite.js
@@ -1,5 +1,30 @@
 function nParite(n, parite) {
- 
+  // Si "parite" est "true" alors calcule la somme de n premiers nombres pairs
+  // SI parité est "false" alors calcul  la somme de n premiers nombres impairs.
+
+  /*Demarche
+
+  // initialiser une variable Sum
+// Verifier si une
+*/
+  let sum = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (parite) {
+
+      if (i % 2 === 0) {
+        sum += i;
+      }
+    } else {
+
+      if (i % 2 !== 0) {
+
+        sum += i;
+      }
+
+    }
+  }
+  return sum;
 }
 
 module.exports = nParite;
--- repeatString.js
+++ repeatString.js
@@ -1,4 +1,9 @@
 function repeatString(string, num) {
   // your code here...
+    let repeatString = '';
+    for (let i = 0; i < num; i++) {
+        repeatString += string;
+    }
+    return repeatString;
 }
-module.exports = leapYear
+module.exports = repeatString
